38af99297af89a49890e51c20d036e871ac572b8
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-11-27 12:50:35 +0200

    Edit app heading

--- react-client-demo/src/components/App.js
+++ react-client-demo/src/components/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
 
 class App extends Component {
   render() {
-    return <h1>Hello!</h1>;
+    return (
+      <Container>
+        <Row className="mt-5">
+          <Col>
+            <h1>Todos React Demo</h1>
+            <p>Todos react demo client for Serverless Rest API demo</p>
+          </Col>
+        </Row>
+      </Container>
+    );
   }
 }
 
