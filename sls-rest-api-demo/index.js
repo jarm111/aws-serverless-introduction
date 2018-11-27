5403662167924b373d20488f4a618c36bcdf9add
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-11-27 23:35:53 +0200

    Move todo routes middleware to a todo controller module

--- sls-rest-api-demo/index.js
+++ sls-rest-api-demo/index.js
@@ -2,10 +2,14 @@ const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-const db = require('./utils/dbConnection')();
-const routes = require('./routes/todoRoutes');
+const todoRoutes = require('./routes/todoRoutes');
 
 app.use(bodyParser.json({ strict: false }));
-routes(app, db);
+
+app.get('/', function(req, res) {
+  return res.send('Hello, this is Serverless REST-API demo!');
+});
+
+todoRoutes(app);
 
 module.exports.handler = serverless(app);
