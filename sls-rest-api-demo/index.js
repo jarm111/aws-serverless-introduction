95707c590ccc4868c890e3424add907ea9305a30
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-11-28 21:34:38 +0200

    Use cors

--- sls-rest-api-demo/index.js
+++ sls-rest-api-demo/index.js
@@ -1,10 +1,12 @@
 const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const todoRoutes = require('./routes/todoRoutes');
 
 app.use(bodyParser.json({ strict: false }));
+app.use(cors());
 
 app.get('/', function(req, res) {
   return res.send('Hello, this is Serverless REST-API demo!');
